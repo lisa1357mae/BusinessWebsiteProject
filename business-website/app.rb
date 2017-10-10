bf2e69c6d435fe523bbb599999845447e0b05a17
Elise Barnes <barnese3@go.stockton.edu>
2017-10-10 16:20:41 -0400

    merge

--- business-website/app.rb
+++ business-website/app.rb
@@ -17,7 +17,7 @@ post '/contact' do
 	from = SendGrid::Email.new(email: params[:email])
 	subject = 'Hello World from the SendGrid Ruby Library!'
 	to = SendGrid::Email.new(email:'embarnese3@hotmail.com')
-	content = SendGrid::Content.new(type:'text/plain', value:'Hello, Email!')
+	content = SendGrid::Content.new(type:'text/plain', value:params["message"])
 	mail = SendGrid::Mail.new(from, subject, to, content)
 	sg = SendGrid::API.new(api_key:ENV['SENDGRID_API_KEY'])
 	response = sg.client.mail._('send').post(request_body: mail.to_json)
